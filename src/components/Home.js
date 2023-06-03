eef5146c106904f670ed100a985d756f80308cf4
Sayan <sayanmajumdar4444@gmail.com>
2023-06-03 16:14:35 +0530

    added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,43 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      This is my home page
+   
+      <>
+    
+    <section id='content' >
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner ">
+    <div class="carousel-item active ">
+      <img class="d-block w-100 " style={{"height":"550px"}} src="https://imechewebresources.blob.core.windows.net/imeche-web-content/journals/coffee-car-home.jpg?sfvrsn=50f5cd12_0" alt="First slide" />
     </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100 " style={{"height":"550px"}} src="https://imechewebresources.blob.core.windows.net/imeche-web-content/journals/coffee-car-home.jpg?sfvrsn=50f5cd12_0" alt="Second slide" />
+    </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100 " style={{"height":"550px"}} src="https://imechewebresources.blob.core.windows.net/imeche-web-content/journals/coffee-car-home.jpg?sfvrsn=50f5cd12_0" alt="Third slide" />
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </section>
+
+<section id='services'>
+
+</section>
+
+    </>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <>
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><img src="https://th.bing.com/th/id/OIP.uwO5sumeTx1kMuZp3l1M7wHaH6?pid=ImgDet&rs=1" class="img-fluid" style={{"width":"45px", "heigth":"45px"}}></img></a>
+  <a class="navbar-brand" href="/"><img src="https://cdn2.vectorstock.com/i/1000x1000/87/51/eco-green-biofuel-logo-design-icon-vector-26788751.jpg" class="img-fluid" style={{"width":"45px", "heigth":"45px"}}></img></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -12,13 +12,13 @@ function Navbar() {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">About <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="/">Link</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
